5de76baa565b3d97035331c3bb633e5dd06e10ea
nschuermannschool <nschuermann@online.gibz.ch>
2022-12-12 02:31:57 +0100

    Add testdata

--- src/EMT_Backend/index.js
+++ src/EMT_Backend/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:8081",
 };
 
 app.use(cors(corsOptions));
@@ -17,9 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // database
 const db = require("./app/models");
-const Role = db.role;
 
-db.sequelize.sync();
+db.sequelize.sync({ alter: true }).then(() => {
+  addTestData();
+});
 // force: true will drop the table if it already exists
 //db.sequelize.sync({force: true}).then(() => {
 //  console.log('Drop and Resync Database with { force: true }');
@@ -32,9 +33,9 @@ app.get("/", (req, res) => {
 });
 
 // routes
-require('./app/routes/auth.routes')(app);
-require('./app/routes/user.routes')(app);
-require('./app/routes/competence.routes')(app);
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+require("./app/routes/competence.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
@@ -42,19 +43,90 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}.`);
 });
 
-function initial() {
-  Role.create({
-    id: 1,
-    name: "user"
+const Role = db.role;
+const Status = db.status;
+const Competence = db.competence;
+const User = db.user;
+const Resource = db.resource;
+
+function addTestData() {
+  Role.findOrCreate({
+    where: { id: 1, name: "teacher" },
   });
- 
-  Role.create({
-    id: 2,
-    name: "moderator"
+
+  Role.findOrCreate({
+    where: { id: 2, name: "admin" },
   });
- 
-  Role.create({
-    id: 3,
-    name: "admin"
+
+  Status.findOrCreate({
+    where: { id: 1, name: "TestStatus1" },
+  });
+
+  Status.findOrCreate({
+    where: { id: 2, name: "TestStatus2" },
+  });
+
+  User.findOrCreate({
+    where: {
+      id: 1,
+      email: "test@test.com",
+      password: "$2a$08$aobMGyJJEby4irclcekV6..wAgJf3/MibRkPGF3eiODbZnvZiMj0q",
+      firstname: "Test",
+      lastname: "ing"
+    }
+  }).then(user => {
+    if(user[1]){
+      user[0].setRoles([1,2]);
+    }
+  })
+
+  User.findOrCreate({
+    where: {
+      id: 2,
+      email: "test2@test.com",
+      password: "$2a$08$aobMGyJJEby4irclcekV6..wAgJf3/MibRkPGF3eiODbZnvZiMj0q",
+      firstname: "Test2",
+      lastname: "ing"
+    }
+  }).then(user => {
+    if(user[1]){
+      user[0].setRoles([1]);
+    }
+  })
+
+  Competence.findOrCreate({
+    where: {
+      id: 1,
+      name: "TestCompetence1",
+      description: "TestCompetence1Desc",
+      statusId: 1,
+      userId: 1
+    },
+  });
+
+  Competence.findOrCreate({
+    where: {
+      id: 2,
+      name: "TestCompetence2",
+      description: "TestCompetence2Desc",
+      statusId: 2,
+      userId: 1
+    },
+  });
+
+  Resource.findOrCreate({
+    where: {
+      id: 1,
+      url: "https://someResource1.com/",
+      competenceId: 1
+    },
+  });
+
+  Resource.findOrCreate({
+    where: {
+      id: 2,
+      url: "https://someResource2.com/",
+      competenceId: 2
+    },
   });
 }
