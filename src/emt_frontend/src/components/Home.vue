7a6f06f5c985532556cbf577fe51952c4d845302
Eugstea1 <andrin.eugster@roche.com>
2022-12-17 18:03:51 +0100

    Create design for home page

--- src/emt_frontend/src/components/Home.vue
+++ src/emt_frontend/src/components/Home.vue
@@ -1,41 +1,51 @@
 <template>
   <div>
     <header>
-      <h3>{{ content }}</h3>
+      <h1>All categories</h1>
     </header>
-  </div>
-  <div v-for="category in categories" :key="category.id">
-    <table>
-      <thead>
-        <tr>
-          <th colspan="3">{{category.name}}</th>
-        </tr>
-        <tr>
-          <th v-for="difficulty in difficulties" :key="difficulty.id">
-            {{ difficulty.name }}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <td v-for="difficulty in difficulties" :key="difficulty.id">
-          <table v-for="competence in competences" :key="competence.id">
-            <div v-if="competence.difficulty.name == difficulty.name && competence.category.name == category.name">
-              <thead>
-                <th>{{ competence.name }}</th>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>{{ competence.description }}</td>
-                </tr>
-                <tr>
-                  <td>{{ competence.category.name }}</td>
-                </tr>
-              </tbody>
-            </div>
+    <div v-for="category in categories" :key="category.id">
+      <div class="card" style="width: 20vw; margin-top: 2vh; margin-bottom: 2vh; width: auto;"> <!-- category card -->
+        <div class="card-body">
+          <h1 class="text-center">{{ category.name }}</h1>
+          <hr />
+          <table style="width: 100%">
+            <thead>
+              <tr>
+                <th v-for="difficulty in difficulties" :key="difficulty.id">
+                  <h3>{{ difficulty.name }}</h3>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <td v-for="difficulty in difficulties" :key="difficulty.id" style="width: 33%; vertical-align: top;">
+                <div v-for="competence in competences" :key="competence.id">
+                  <div v-if="competence.difficulty.name == difficulty.name && competence.category.name == category.name"
+                    class="card m-1 p-1" style="vertical-align: top;"> <!-- competence card -->
+                    <div style="display: flex; justify-content: space-between;">
+                      <h5>{{ competence.name }}</h5>
+                      <div v-if="competence.user_competences.status.name == 'ToDo'" style="color: orange;">
+                        {{ competence.user_competences.status.name }}
+                        <img src="../assets/ToDo.png" width="20" height="20">
+                      </div>
+                      <div v-if="competence.user_competences.status.name == 'In Progress'" style="color: blue;">
+                        {{ competence.user_competences.status.name }}
+                        <img src="../assets/In_Progress.png" width="20" height="20">
+                      </div>
+                      <div v-if="competence.user_competences.status.name == 'Done'" style="color: green;">
+                        {{ competence.user_competences.status.name }}
+                        <img src="../assets/Done.png" width="20" height="20">
+                      </div>
+                    </div>
+                    <p>{{ competence.description }}</p>
+                    <button type="button" class="btn btn-outline-dark">Go to competence</button>
+                  </div>
+                </div>
+              </td>
+            </tbody>
           </table>
-        </td>
-      </tbody>
-    </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -82,8 +92,8 @@ export default {
           (error.response &&
             error.response.data &&
             error.response.data.message) ||
-            error.message ||
-            error.toString()
+          error.message ||
+          error.toString()
         );
       }
     );
@@ -96,8 +106,8 @@ export default {
           (error.response &&
             error.response.data &&
             error.response.data.message) ||
-            error.message ||
-            error.toString()
+          error.message ||
+          error.toString()
         );
       }
     );
@@ -110,8 +120,8 @@ export default {
           (error.response &&
             error.response.data &&
             error.response.data.message) ||
-            error.message ||
-            error.toString()
+          error.message ||
+          error.toString()
         );
       }
     );
