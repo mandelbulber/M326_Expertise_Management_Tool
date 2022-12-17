2cea0337330c2204346da4d7fee4ed940bc327ff
Eugstea1 <andrin.eugster@roche.com>
2022-12-18 00:30:24 +0100

    Design edit competence page

--- src/emt_frontend/src/components/EditCompetence.vue
+++ src/emt_frontend/src/components/EditCompetence.vue
@@ -1,50 +1,38 @@
 <template>
-  <div>
-    <p>{{ competence }}</p>
-
-    <div><label>Name</label></div>
-    <div>
-      <input type="text" placeholder="Name" v-model="competence.name" />
-    </div>
-
-    <div><label>Description</label></div>
-    <div>
-      <input
-        type="text"
-        placeholder="Description"
-        v-model="competence.description"
-      />
-    </div>
-
-    <div><label>Category</label></div>
-    <div>
-      <v-select
-        :options="categories"
-        label="name"
-        v-model="competence.category"
-        :clearable="false"
-      ></v-select>
-    </div>
-
-    <div><label>Difficulty</label></div>
-    <div>
-      <v-select
-        :options="difficulties"
-        label="name"
-        v-model="competence.difficulty"
-        :clearable="false"
-      ></v-select>
-    </div>
-
-    <div><label>Ressources</label></div>
-    <div><button @click="addResource(competence.id)">Add Resource</button></div>
-    <div v-for="resource in competence.resources" :key="resource.id">
-      <input type="url" placeholder="url" v-model="resource.url" />
-      <button @click="deleteResource(resource)">Delete</button>
-    </div>
-
-    <div>
-      <button @click="editCompetence(competence)">Save</button>
+  <div style="margin: 1em; display: flex; align-items: center; flex-direction: column; min-height: 84.5vh;;">
+    <h1>Edit competence '{{ competence.name }}'</h1>
+    <div class="card" style="width: 50vw;">
+      <div style="margin: 1em;">
+        <label class="form-label">Name</label>
+        <input type="text" class="form-control" placeholder="Name" v-model="competence.name" />
+      </div>
+      <div style="margin: 1em">
+        <label class="form-label">Description</label>
+        <input type="text" placeholder="Description" v-model="competence.description" class="form-control" />
+      </div>
+      <div style="margin: 1em; display: flex; justify-content: space-between;">
+        <div style="width: 47%">
+          <label class="form-label">Category</label>
+          <v-select :options="categories" label="name" v-model="competence.category" :clearable="false"></v-select>
+        </div>
+        <hr />
+        <div style="width: 47%">
+          <label class="form-label">Difficulty</label>
+          <v-select :options="difficulties" label="name" v-model="competence.difficulty" :clearable="false"></v-select>
+        </div>
+      </div>
+      <hr />
+      <div style="margin: 1em; margin-top: 0;">
+        <div style="display: flex; justify-content: space-between;">
+          <label class="form-label">Ressources</label>
+          <button @click="addResource(competence.id)" class="btn btn-outline-dark">Add Resource</button>
+        </div>
+        <div v-for="resource in competence.resources" :key="resource.id" style="display: flex; justify-content: space-between; margin: 0.5em 0 0.5em 0.5em">
+          <input type="url" placeholder="url" v-model="resource.url" class="form-control" style="width: 90%;"/>
+          <button @click="deleteResource(resource)" class="btn btn-outline-danger">Delete</button>
+        </div>
+        <button @click="editCompetence(competence)" class="btn btn-outline-dark" style="width: 90%; margin: 1% 5% 1% 5%;">Save</button>
+      </div>
     </div>
   </div>
 </template>
@@ -80,8 +68,8 @@ export default {
           (error.response &&
             error.response.data &&
             error.response.data.message) ||
-            error.message ||
-            error.toString()
+          error.message ||
+          error.toString()
         );
       }
     );
@@ -94,8 +82,8 @@ export default {
           (error.response &&
             error.response.data &&
             error.response.data.message) ||
-            error.message ||
-            error.toString()
+          error.message ||
+          error.toString()
         );
       }
     );
@@ -108,8 +96,8 @@ export default {
           (error.response &&
             error.response.data &&
             error.response.data.message) ||
-            error.message ||
-            error.toString()
+          error.message ||
+          error.toString()
         );
       }
     );
@@ -134,7 +122,7 @@ export default {
         ResourceService.deleteById(resource.id).then((res) => {
           console.log(res);
           if (this.competence.resources.indexOf(resource) > -1) {
-            this.competence.resources.splice(this.competence.resources.indexOf(resource),1);
+            this.competence.resources.splice(this.competence.resources.indexOf(resource), 1);
           }
         });
       } else {
